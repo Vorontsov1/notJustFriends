af4b7315f1f567b90437582e1903a9f323c5dc42
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-05 22:04:01 +0300

    imagePicker

--- src/screens/CreatPostScreen.js
+++ src/screens/CreatPostScreen.js
@@ -1,6 +1,8 @@
 import { Text, StyleSheet, View, Image, TextInput, Button, KeyboardAvoidingView, Platform } from "react-native";
 import { useState } from "react";
 // import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 
 
@@ -14,33 +16,58 @@ const user = {
 
 
 const CreatPostScreen = () => {
-    const [description, setDescription] = useState("hello world");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState(null);
     // const insets = useSafeAreaInsets();
     
    
     const onSubmit = () => {
         console.warn('submitted');
     }
+
+
+      const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.canceled) {
+          setImage(result.uri);
+        }
+      };
     return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={[styles.container, { marginBottom: 10 }]}
-            contentContainerStyle={styles.container}
-            // keyboardVerticalOffset={150}
-        >
-          <View style={styles.header}>
-            <Image source={{ uri: user.image }} style={styles.image} />
-            <Text style={styles.name}>{user.name}</Text>
-          </View>
-          <TextInput
-            value={description}
-            onChangeText={setDescription}
-            placeholder="What's happening?"
-            multiline
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={[styles.container, { marginBottom: 10 }]}
+        contentContainerStyle={styles.container}
+        // keyboardVerticalOffset={150}
+      >
+        <View style={styles.header}>
+          <Image source={{ uri: user.image }} style={styles.profileImage} />
+          <Text style={styles.name}>{user.name}</Text>
+          <Entypo
+            onPress={pickImage}
+            name="images"
+            size={24}
+            color="limegreen"
+            style={styles.icon}
           />
-          <View style={styles.buttonContainer}>
-            <Button title="Post" onPress={onSubmit} />
-          </View>
+        </View>
+        <TextInput
+          value={description}
+          onChangeText={setDescription}
+          placeholder="What's happening?"
+          multiline
+            />
+            <Image source={{ uri: image }} style={styles.image } />
+        <View style={styles.buttonContainer}>
+          <Button title="Post" onPress={onSubmit} />
+        </View>
       </KeyboardAvoidingView>
     );
 };
@@ -59,19 +86,28 @@ const styles = StyleSheet.create({
     width: "100%",
     marginBottom: 15,
   },
-  image: {
+  profileImage: {
     width: 40,
     height: 40,
     borderRadius: 30,
     marginRight: 10,
+    },
+    image: {
+        width: "100%",
+        alignSelf: "center",
+        aspectRatio: 4 / 3,
   },
   name: {
     fontWeight: "bold",
   },
-    buttonContainer: {
-      marginTop: 'auto',
-        backgroundColor: "#FF6C00",
-   borderRadius: 20,
+  buttonContainer: {
+    marginTop: "auto",
+    marginBottom: 30,
+    backgroundColor: "#FF6C00",
+    borderRadius: 20,
+  },
+  icon: {
+    marginLeft: "auto",
   },
 });
 
